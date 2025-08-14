3847b985a9dcde87ac516ba809a5601c058c0a16
pawandeep16 <thandipawandeep@gmail.com>
2025-08-13 22:22:49 -0400

    updated code

--- next.config.js
+++ next.config.js
@@ -12,20 +12,6 @@ const nextConfig = {
     PROVIDENCE_USERNAME: process.env.PROVIDENCE_USERNAME,
     PROVIDENCE_PASSWORD: process.env.PROVIDENCE_PASSWORD,
   },
-  // Add the webpack config to handle serverless environments
-  webpack: (config, { isServer }) => {
-    // Only apply this configuration on the server-side
-    if (isServer) {
-      // This part tells Next.js not to bundle these packages, as they are
-      // required at runtime in the serverless function.
-      config.externals.push({
-        '@sparticuz/chromium': 'commonjs @sparticuz/chromium',
-        'chrome-aws-lambda': 'commonjs chrome-aws-lambda',
-      });
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
