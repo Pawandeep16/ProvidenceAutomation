0848ebb25da7011853134d77739d6a1c54690928
pawandeep16 <thandipawandeep@gmail.com>
2025-08-13 22:43:03 -0400

    updated code

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: true,
-  },
-  env: {
-    GOOGLE_SHEETS_API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
-    GOOGLE_SERVICE_ACCOUNT_EMAIL: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    PROVIDENCE_USERNAME: process.env.PROVIDENCE_USERNAME,
-    PROVIDENCE_PASSWORD: process.env.PROVIDENCE_PASSWORD,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true,
+    outputFileTracingIncludes: {
+      // Ensure binaries get packaged with the serverless function
+      '/**': ['./node_modules/@sparticuz/chromium/bin/**'],
+    },
+  },
+  env: {
+    GOOGLE_SHEETS_API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
+    GOOGLE_SERVICE_ACCOUNT_EMAIL: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    PROVIDENCE_USERNAME: process.env.PROVIDENCE_USERNAME,
+    PROVIDENCE_PASSWORD: process.env.PROVIDENCE_PASSWORD,
+  },
 };
 
 export default nextConfig;
